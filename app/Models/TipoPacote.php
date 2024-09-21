e68517ead615217da6f01b5c5ef0aa12622a7938
lucasvarino <lucasvarino@gmail.com>
2024-09-21 12:55:23 -0300

    Adding Filament and models

---
+++ app/Models/TipoPacote.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoPacote extends Model
+{
+    use HasFactory;
+}
